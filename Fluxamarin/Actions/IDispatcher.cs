07315c260abf00aba9f2cd307de16d122e4570d3
Daniel Bautista Hernández <Danielbahe@gmail.com>
2018-10-08 08:18:36 +0200

    First implementation

---
+++ Fluxamarin/Actions/IDispatcher.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Fluxamarin.Actions
+{
+	public interface IDispatcher
+	{
+		void Dispatch(Type actionType, object data = null);
+		Task DispatchAsync(Type actionType, object data = null);
+	}
+}
---
+++ Fluxamarin/Views/WidgetState.cs
@@ -0,0 +1,9 @@
+﻿namespace Fluxamarin.Views
+{
+	public enum WidgetState
+	{
+		Active = 0,
+		Inactive = 1,
+		Destroyed = 2
+	}
+}
