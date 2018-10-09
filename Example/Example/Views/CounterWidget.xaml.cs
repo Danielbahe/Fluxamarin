0030bf15b41bf46e3bd5116f1102df20f8473a0d
Daniel Bautista Hernández <Danielbahe@gmail.com>
2018-10-09 12:22:30 +0200

    counter example

---
+++ Example/Example/Views/CounterWidget.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Example.Actions;
+using Example.Models;
+using Fluxamarin.Stores;
+using Fluxamarin.Views;
+using Xamarin.Forms.Xaml;
+
+namespace Example.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class CounterWidget : StatefulWidget<AppState>
+	{
+		private int _counter;
+
+		public int Counter
+		{
+			get => _counter;
+			set
+			{
+				_counter = value;
+				OnPropertyChanged(nameof(Counter));
+			}
+		}
+
+		public CounterWidget(BasePage<AppState> parentPage) : base(parentPage)
+		{
+			BindingContext = this;
+			InitializeComponent();
+			Initialize();
+		}
+
+		private void Initialize()
+		{
+			//In this example is the same that  state changed
+			OnStateChanged(Store, Store.AppState);
+		}
+
+		private void Button_OnClicked(object sender, EventArgs e)
+		{
+			Counter++;
+		}
+
+		protected override void OnStateChanged(ReadonlyStore<AppState> store, AppState newState)
+		{
+			Counter = newState.Counter;
+		}
+
+		private void Button_OnClickedWithState(object sender, EventArgs e)
+		{
+			Dispatcher.Dispatch(typeof(CounterAction), Counter + 1);
+		}
+	}
+}
---
+++ Example/Example/Views/Pages/CounterPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using Example.Models;
+using Fluxamarin.Stores;
+using Fluxamarin.Views;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Example.Views.Pages
+{
+	public partial class CounterPage : BasePage<AppState>
+	{
+		public CounterPage(ReadonlyStore<AppState> store) : base(store)
+		{
+			InitializeComponent();
+
+			Content = new StackLayout()
+			{
+				Children = { new CounterWidget(this) }
+			};
+		}
+
+		protected override void OnStateChanged(ReadonlyStore<AppState> store, AppState newState)
+		{
+
+		}
+	}
+}
